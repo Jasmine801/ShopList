8646398c83b9a79b9e7dd06b2a23722e2e60c991
emilm <emil.mamedlov7779@gmail.com>
2023-02-23 18:12:05 +0300

    NewNote & Delete

--- app/src/main/java/com/example/shoppinglist/db/Dao.kt
+++ app/src/main/java/com/example/shoppinglist/db/Dao.kt
@@ -10,6 +10,10 @@ import kotlinx.coroutines.flow.Flow
 interface Dao {
     @Query ("SELECT * FROM note_list")
     fun getAllNotes(): Flow<List<NoteItem>>
+
     @Insert
     suspend fun insertNote(note: NoteItem)
+
+    @Query ("DELETE FROM note_list WHERE id IS :id")
+    suspend fun deleteNote(id: Int)
 }
